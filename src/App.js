04d9c8b9e8f2c8dc7eefc43f6e4f6635689fb9b3
Victor Agustín Diaz Mendez <mars.vic5@gmail.com>
2020-05-12 00:19:23 -0600

    use hook instead of class

--- src/App.js
+++ src/App.js
@@ -1,61 +1,33 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+// import Axios from 'axios'
+import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newComment: '',
-      comments: [{ id: 1, comment: 'Foo' }],
-    }
-  }
+function App () {
+  const [comments] = useState([])
+  const [newComment, setNewComment] = useState('')
 
-  componentDidMount () {
-    Axios.get('http://localhost:3000/comments')
-      .then((res) => {
-        const { data: { comments } } = res;
-        this.setState({ comments });
-      })
-      .catch(err => console.error);
-  }
+  useEffect(() => {
+    console.log('use effect called')
+  })
 
-  onClickBtn = async () => {
-    const { newComment } = this.state;
-    const { data: comment } = await Axios.post('http://localhost:3000/comments', {
-      comment: newComment
-    });
-    this.setState((state) => ({
-      comments: [...state.comments, comment]
-    }))
-  }
-
-  onChangeNewComment = (ev) => {
-    this.setState({ newComment: ev.target.value })
-  }
-
-  render () {
-    const { comments } = this.state
-    return (
-      <div className="container">
-        <input
-          type="text"
-          value={this.state.newComment}
-          onChange={this.onChangeNewComment}
-        />
-        <button onClick={this.onClickBtn}>Comment</button>
-        {comments.map(({ id, comment, votes }) => (
-          <div className="card" key={id}>
-            <div>
-              {comment}
-            </div>
-            <small>{votes}</small>
+  return (
+    <div className='container'>
+      <input
+        type='text'
+        value={newComment}
+        onChange={(ev) => setNewComment(ev.target.value)}
+      />
+      <button onClick={() => {}}>Comment</button>
+      {comments.map(({ id, comment, votes }) => (
+        <div className='card' key={id}>
+          <div>
+            {comment}
           </div>
-        ))}
-      </div>
-    );
-  }
+          <small>{votes}</small>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default App;
+export default App

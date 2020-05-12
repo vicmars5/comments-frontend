5fb32eb14f379cdf17e7cccced3fe9e013d6a13c
Victor Agustín Diaz Mendez <mars.vic5@gmail.com>
2020-05-12 02:28:22 -0600

    support for remote state added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,78 @@
 import React, { useState, useEffect } from 'react'
-// import Axios from 'axios'
+import useSWR from 'swr' // remote data lib
+import Axios from 'axios'
 import './App.css'
 
-function App () {
-  const [comments] = useState([])
-  const [newComment, setNewComment] = useState('')
+const http = Axios.create({
+  baseURL: 'http://localhost:3000'
+})
 
-  useEffect(() => {
-    console.log('use effect called')
-  })
+async function getComments () {
+  const res = await http.get('/comments')
+  return res.data.comments
+}
+
+async function createComment (comment) {
+  const res = await http.post('/comments', { comment })
+  return res.data
+}
+
+function useCommentsState () {
+  const { data: comments, error, mutate } = useSWR('/comments', getComments)
+
+  async function addComment (comment) {
+    mutate(async (comments) => {
+      return [...comments, await createComment(comment)]
+    })
+  }
+
+  return { comments, error, addComment }
+}
+
+function CommentsList (props) {
+  const { comments } = props
 
   return (
-    <div className='container'>
+    comments.map(({ id, comment, votes }) => (
+      <div className='card' key={id}>
+        <div>
+          {comment}
+        </div>
+        <small>{votes}</small>
+      </div>
+    ))
+  )
+}
+
+function CommentInput (props) {
+  const [newComment, setNewComment] = useState('')
+  const { addComment } = props
+
+  return (
+    <>
       <input
         type='text'
         value={newComment}
         onChange={(ev) => setNewComment(ev.target.value)}
       />
-      <button onClick={() => {}}>Comment</button>
-      {comments.map(({ id, comment, votes }) => (
-        <div className='card' key={id}>
-          <div>
-            {comment}
-          </div>
-          <small>{votes}</small>
-        </div>
-      ))}
+      <button onClick={() => addComment(newComment)}>Comment</button>
+    </>
+  )
+}
+
+function App () {
+  const { comments, error, addComment } = useCommentsState()
+
+  if (error) {
+    return <div>Something failed. Please see console for more info</div>
+  }
+
+  if (!comments) return <div>Requesting comments...</div>
+
+  return (
+    <div className='container'>
+      <CommentInput addComment={addComment} />
+      <CommentsList comments={comments} />
     </div>
   )
 }
